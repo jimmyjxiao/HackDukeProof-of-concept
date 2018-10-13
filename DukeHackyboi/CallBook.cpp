c9ba54a978ec298d1eb963c51a3b1c934827aac6
Jimmy <jimmyjxiao@outlook.com>
2018-10-13 18:35:33 -0400

    Stuff

--- DukeHackyboi/CallBook.cpp
+++ DukeHackyboi/CallBook.cpp
@@ -1,4 +1,70 @@
 #include "book\order_book.h"
-
+#include "CallBook.h"
+#include "server_HTTP.hpp"
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
 using namespace liquibook;
+using namespace book;
+using namespace boost::property_tree;
+using namespace std;
 
+
+
+
+liquibook::book::Quantity watt_order::order_qty() const
+{
+	return num_watts;
+}
+
+void main()
+{
+	auto book = EnergyBook();
+	SimpleWeb::Server<SimpleWeb::HTTP> server;
+	server.resource["^/newbuy$"]["POST"] = [&book](std::shared_ptr<SimpleWeb::Server<SimpleWeb::HTTP>::Response> response, std::shared_ptr < SimpleWeb::Server<SimpleWeb::HTTP>::Request> request) {
+
+		try {
+			ptree pt;
+
+			read_json(request->content, pt);
+
+
+			book.add(std::make_shared<buy_order>(pt.get<double>("x_loc"), pt.get<double>("y_loc"), pt.get<unsigned int>("watts")));
+
+			*response << "HTTP/1.1 200 OK\r\n";
+
+		}
+
+		catch (const std::exception &e) {
+
+			*response << "HTTP/1.1 400 Bad Request\r\nContent-Length: " << strlen(e.what()) << "\r\n\r\n"
+
+				<< e.what();
+
+		}
+	};
+	server.resource["^/newsell$"]["POST"] = [&book](std::shared_ptr<SimpleWeb::Server<SimpleWeb::HTTP>::Response> response, std::shared_ptr < SimpleWeb::Server<SimpleWeb::HTTP>::Request> request) {
+
+		try {
+			ptree pt;
+
+			read_json(request->content, pt);
+
+			book.add(std::make_shared<sell_order>(pt.get<unsigned int>("price"), pt.get<double>("x_loc"), pt.get<double>("y_loc"), pt.get<unsigned int>("watts")));
+
+
+
+
+			*response << "HTTP/1.1 200 OK\r\n";
+
+		}
+
+		catch (const std::exception &e) {
+
+			*response << "HTTP/1.1 400 Bad Request\r\nContent-Length: " << strlen(e.what()) << "\r\n\r\n"
+
+				<< e.what();
+
+		}
+	};
+	server.start();
+}
--- DukeHackyboi/CallBook.h
+++ DukeHackyboi/CallBook.h
@@ -1,1 +1,50 @@
 #pragma once
+#include "book\order_book.h"
+#include "book\order.h"
+class watt_order : public liquibook::book::Order
+{
+protected:
+	double x_loc;
+	double y_loc;
+	unsigned int num_watts;
+	watt_order(float x_location, float y_locaction, unsigned int Num_Watts) : x_loc(x_location), y_loc(y_locaction), num_watts(Num_Watts)
+	{}
+public:
+	virtual liquibook::book::Quantity order_qty() const override;
+};
+class buy_order : public watt_order
+{
+public:
+	// Inherited via Order
+	virtual bool is_buy() const override
+	{
+		return true;
+	}
+	virtual liquibook::book::Price price() const override
+	{
+		return 0;
+	}
+	buy_order(float x_location, float y_locaction, unsigned int Num_Watts) : watt_order(x_location, y_locaction, Num_Watts) {}
+};
+class sell_order : public watt_order
+{
+private:
+	unsigned int cents;
+	bool is_limit = true;
+public:
+	virtual bool is_buy() const override
+	{
+		return false;
+	}
+	virtual liquibook::book::Price price() const override
+	{
+		if (is_limit)
+			return cents;
+		else
+			return 0;
+	}
+	sell_order(float x_location, float y_locaction, unsigned int Num_Watts, unsigned int price_in_cents) : watt_order(x_location, y_locaction, Num_Watts) {}
+};
+
+typedef liquibook::book::OrderBook<std::shared_ptr<watt_order>> EnergyBook;
+void main();
